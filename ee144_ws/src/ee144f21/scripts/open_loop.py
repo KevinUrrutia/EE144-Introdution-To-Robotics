1c0c2af5f796c22eaeb4280ae616e115fc7145f3
KevinUrrutia <kurru006@ucr.edu>
2021-10-03 22:06:35 -0700

    lab 2

--- ee144_ws/src/ee144f21/scripts/open_loop.py
+++ ee144_ws/src/ee144f21/scripts/open_loop.py
@@ -2,36 +2,55 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from gazebo_msgs.msg import ModelStates
+import time
 from math import pi
+from nav_msgs.msg import Odometry
+#from tf.transformations import euler_from_quaternion
 
 class TurtleBot():
     def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
         rospy.init_node("turtlebot_move")
         rospy.loginfo("Press Ctrl + C to terminate")
         self.vel_pub = rospy.Publisher("cmd_vel_mux/input/navi", Twist, queue_size=10)
-        self.rate = rospy.Rate(10)
+	sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.Position)
+        self.rate = rospy.Rate(10) 
         self.run()
+      
+    def Position(self, msg):
+        print msg.pose
 
     def run(self):
         vel = Twist()
-        vel.linear.x = 0
-        vel.angular.z = 0
+        
         i = 0
         while not rospy.is_shutdown():
-            if i is not 4:
-                vel.linear.x += 4
-                vel.angular.z += 90
-                i += 1
-                rospy.spin(10)
-            else:
-                vel.linear.x = 0
-                vel.angular.z = 0
-                val = raw_input("Finished rotation continue y/n?")
-                if val == 'y':
-                    i = 0
-                elif val == 'n':
-                    break
+            if i < 4:      
+                time_end = time.time() + 14
+	        while time.time() < time_end:
+                    vel.linear.x = 0.5
+                    vel.angular.z = 0
+                    self.vel_pub.publish(vel)
+                    self.rate.sleep()
 
+      	        time.sleep(1)
+	
+                time_end = time.time() + 3
+                while time.time() < time_end:
+                    vel.linear.x = 0
+                    vel.angular.z = pi/4
+                    self.vel_pub.publish(vel)
+                    self.rate.sleep()  
+         
+                time.sleep(1)  
+                i += 1
+            else:
+                break
+
+            
 if __name__ == '__main__':
     try:
         whatever = TurtleBot()
