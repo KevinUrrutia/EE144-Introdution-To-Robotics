5d618f414dcb2ab771cd33d59267d466e896c7f2
KevinUrrutia <kurru006@ucr.edu>
2021-10-10 11:54:10 -0700

    creating of closed loop

---
+++ ee144_ws/src/ee144f21/scripts/closed_loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from math import pi, sqrt, atan2, cos, sin
+import numpy as np
+
+import rospy
+import tf
+from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
+from geometry_msg.msg import Twist, Pose2D
+
+
