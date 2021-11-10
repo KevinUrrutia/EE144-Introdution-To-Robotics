c4f3af4f38b801bfb5c8d5e0a0c00c6af16577ff
KevinUrrutia <kurru006@ucr.edu>
2021-11-10 14:58:30 -0800

    Lab 6 update

--- ee144_ws/src/ee144f21/scripts/trajectory_generation.py
+++ ee144_ws/src/ee144f21/scripts/trajectory_generation.py
@@ -54,8 +54,9 @@ class Turtlebot():
         #generate polynomial trajectory and move to current_waypoint 
         #next_waypoint is to help determine the velocity to pass current waypoint
       
+        print("**********************************************")
         #calculate the orientation of the robot
-        theta = atan2((next_waypoint[0] - current_waypoint[0]),(next_waypoint[1] - current_waypoint[1]))
+        theta = atan2((next_waypoint[1] - self.previous_waypoint[1]),(next_waypoint[0] - self.previous_waypoint[0]))
         
         #decompose the velocity for the boundary conditions
         #check if last waypoint
@@ -63,31 +64,39 @@ class Turtlebot():
         vy_end = self.vel_ref * sin(theta)
 
         #change to set the trajectory
-        T = 3
+        T = 4
 
         #coeffcient array calculate from polynomial function
         c_x = self.polynomial_time_scaling_3rd_order(self.previous_waypoint[0], self.previous_velocity[0], current_waypoint[0], vx_end, T)
+        print(c_x)
         c_y = self.polynomial_time_scaling_3rd_order(self.previous_waypoint[1], self.previous_velocity[1], current_waypoint[1], vy_end, T)
+        print(c_y)
         calc_vx = 0
         calc_vy = 0
         #calculate the changing velocity based on time
-        for i in range(T * 20):
+        for i in range(T * 25):
             calc_vx = c_x[1] + 2*c_x[2]*(i/10) + 3*c_x[3]*pow((i/10),2)
             calc_vy = c_y[1] + 2*c_y[2]*(i/10) + 3*c_y[3]*pow((i/10),2)
 	    velocity  = sqrt(pow(calc_vy, 2) + pow(calc_vx, 2))
             self.vel.linear.x = velocity
 
+            vel_theta = atan2(calc_vy, calc_vx)
+            print(vel_theta)
+
             #compute the delta theta
-            diff_theta = theta - self.pose.theta
+            diff_theta = vel_theta - self.pose.theta
+            print(diff_theta)
+            print(self.pose.theta)
             #change the values of Kp
-            kp = 7	    
+            kp = 10    
             P_term = kp * diff_theta
+            #print(P_term)
 
             self.vel.angular.z = P_term
+            #self.vel.angular.z = 0
             self.vel_pub.publish(self.vel)
             self.rate.sleep()
 
-	print("***********************************************************************")
         self.previous_waypoint[0] = current_waypoint[0]
         self.previous_waypoint[1] = current_waypoint[1]
 
@@ -100,7 +109,9 @@ class Turtlebot():
         #       T: the desired time to complete this segment of trajectory (seconds)
         #output: the coefficients of this polynomial
         x_array = np.array([[p_start], [p_end], [v_start], [v_end]]) #initial condition array
+        #print(x_array)
         T_mat = np.array([[0,0,0,1], [pow(T,3),pow(T,2),T,1], [0,0,1,0], [3*pow(T,2),2*T,1,0]]) # coefficient matrix
+        #print(T_mat)
  
         T_mat_inv = np.linalg.inv(T_mat) #find the inverse coefficient matrix
         coefficients = np.dot(T_mat_inv, x_array)  #solve for the coefficient array 
